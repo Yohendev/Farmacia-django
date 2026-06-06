40f71d598b74e1378ee441d77eade16a5fc98dd4
Yohendev <limajoaovitor069@gmail.com>
2026-06-06 11:52:46 -0300

    Atualização admin

--- website/admin.py
+++ website/admin.py
@@ -3,7 +3,28 @@ from .models import Medicamento
 
 @admin.register(Medicamento)
 class MedicamentoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'categoria', 'preco', 'quantidade', 'atualizado_em')
-    list_filter = ('categoria',)
+    list_display = ('nome', 'apresentacao', 'categoria', 'tarja', 'preco', 'quantidade', 'em_estoque')
+    
     search_fields = ('nome', 'apresentacao')
+    
+    list_filter = ('categoria', 'tarja', 'em_estoque')
+    
+    list_editable = ('preco', 'quantidade', 'em_estoque')
+    
     readonly_fields = ('criado_em', 'atualizado_em')
+    
+    fieldsets = (
+        ('Informações Principais', {
+            'fields': ('nome', 'apresentacao', 'categoria', 'tarja')
+        }),
+        ('Estoque e Precificação', {
+            'fields': ('preco', 'quantidade', 'em_estoque')
+        }),
+        ('Mídia', {
+            'fields': ('imagem',)
+        }),
+        ('Auditoria', {
+            'fields': ('criado_em', 'atualizado_em'),
+            'classes': ('collapse',)
+        }),
+    )
